f15cad72ef49788c24da6a2c14bd9f69047c08b1
ArianDaSilva <dasilvaarian2@gmail.com>
2023-03-20 11:20:26 -0300

    first commit

---
+++ testing/src/main/java/com/mycompany/testing/Main.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.testing;
+
+/**
+ *
+ * @author Arian
+ */
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Hola Mundo.");
+    }
+}
+    
